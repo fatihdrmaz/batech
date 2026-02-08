69ed4961df8f5e76ae73397205564b1c7e3f289c
Fatih Durmaz <fdurmaz@gmail.com>
2026-02-08 13:43:16 +0300

    commit mes2s2a2g2222e2222222

--- i18n.ts
+++ i18n.ts
@@ -6,10 +6,12 @@ export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = "tr";
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as Locale)) notFound();
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  if (!locale || !locales.includes(locale as Locale)) notFound();
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default,
   };
 });
